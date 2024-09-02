8539a1108b15f41871b27f740fad52014e67aab8
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-02 11:30:26 -0400

    Creating Documents and Testing the Model

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE?.replace(
-  "<PASSWORD>",
+  "<DB_PASSWORD>",
   process.env.DATABASE_PASSWORD || ""
 );
 
@@ -16,7 +16,7 @@ if (!DB) {
 mongoose
   .connect(DB)
   .then(() => console.log("DB connection successful!"))
-  .catch(() => console.log("DB connection unsuccessful!"));
+  .catch((err) => console.log("DB connection error: ", err));
 
 // Define an interface for the Tour document
 interface ITour extends Document {
@@ -44,6 +44,21 @@ const tourSchema: Schema<ITour> = new mongoose.Schema({
 
 // Create the model with a generic type argument
 const Tour: Model<ITour> = mongoose.model<ITour>("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
