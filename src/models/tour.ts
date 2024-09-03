487d2f080b90e643d7180939975a3a1a5581df29
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-03 14:50:07 -0400

    Modified Tour schema

--- src/models/tour.ts
+++ src/models/tour.ts
@@ -3,8 +3,20 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 // Define an interface for the Tour document
 export interface ITour extends Document {
   name: string;
-  rating: number;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: "easy" | "medium" | "difficult";
+  ratingsAverage: number;
+  ratingQuantity: number;
   price: number;
+  priceDiscount: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: Date[];
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 // Define the schema with TypeScript type annotations
@@ -14,14 +26,53 @@ const tourSchema: Schema<ITour> = new mongoose.Schema(
       type: String,
       required: [true, "A tour must have a name"],
       unique: true,
+      trim: true,
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a groupSize"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+    },
+    ratingsAverage: {
       type: Number,
       default: 4.5,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
     },
     price: {
       type: Number,
       required: [true, "A tour must have a price"],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a summary"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"],
+    },
+    images: {
+      type: [String],
+    },
+    startDates: {
+      type: [Date],
     },
   },
   { strict: true, timestamps: true }
