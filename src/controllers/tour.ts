55266c973e175bd36b4b14e29051c0e094b3c7bf
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-03 13:04:33 -0400

    Modified delete, update, get all, and get one tour actions

--- src/controllers/tour.ts
+++ src/controllers/tour.ts
@@ -7,17 +7,26 @@ interface RequestWithBody<T> extends Request {
 }
 
 // Handler to get all tours
-export const getTours = (req: Request, res: Response): void => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet defined." });
+export const getTours = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const tours = await Tour.find();
+    res
+      .status(200)
+      .json({ status: "success", results: tours.length, data: { tours } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err });
+  }
 };
 
 // Handler to get a specific tour by ID
-export const getTour = (req: Request, res: Response): void => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet defined." });
+export const getTour = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { params } = req;
+    const tour = await Tour.findById(params.id);
+    res.status(200).json({ status: "success", data: { tour } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err });
+  }
 };
 
 // Handler to create a new tour
@@ -34,15 +43,32 @@ export const createTour = async (
 };
 
 // Handler to update a tour (not yet defined)
-export const updateTour = (req: Request, res: Response): void => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet defined." });
+export const updateTour = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { params, body } = req;
+    const tour = await Tour.findByIdAndUpdate(params.id, body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ status: "success", data: { tour } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err });
+  }
 };
 
 // Handler to delete a tour (not yet defined)
-export const deleteTour = (req: Request, res: Response): void => {
-  res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet defined." });
+export const deleteTour = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { params } = req;
+    await Tour.findByIdAndDelete(params.id);
+    res.status(200).json({ status: "success" });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err });
+  }
 };
