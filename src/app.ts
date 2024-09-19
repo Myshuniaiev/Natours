8c6c9c262f4554d5c7439b3a1d40613e78c102b6
Oleksii Myshuniaiev <67588654+Myshuniaiev@users.noreply.github.com>
2024-09-18 22:35:02 -0400

    Error handling (#2)
    * handling unhandled routes
    
    * refactoring error handling related functions
    
    * catching errors in async functions
    
    * removal of async on aliasTopTours func
    
    * added 404 error on tour route
    
    * divided errors into dev and prod
    
    * handling invalid database IDs
    
    * modified the start command with the production env
    
    * added build script
    
    * handling duplicated database fields
    
    * handling mongoose validation errors
    
    * handling errors outside express: unhandled rejections
    
    * handling uncaught exceptions

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,34 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import morgan from "morgan";
+import path from "path";
+
+import AppError from "./utils/appError";
 
 import tourRouter from "./routes/tour";
 import userRouter from "./routes/user";
+import { globalErrorHandler } from "./controllers/error";
 
 const app: Application = express();
 
+// Log development environment requests with morgan
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Parse JSON bodies and serve static files
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
+// API routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+// Handle undefined routes with custom error
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/controllers/error.ts
@@ -0,0 +1,71 @@
+import { NextFunction, Request, Response } from "express";
+import AppError, { ExtendedError } from "../utils/appError";
+
+const handleCastErrorDB = (err: ExtendedError) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicatedFieldsDB = (err: ExtendedError) => {
+  const regex = /\{\s*[^:]+:\s*\\"([^\\"]+)\\"/;
+  const match = err.errorResponse.errmsg.match(regex);
+  const extractedValue = match ? match[1] : null;
+  const message = `Duplicated field value ${extractedValue}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err: ExtendedError) => {
+  // console.log(err.error)
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid Validation Error. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err: AppError, res: Response) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+
+const sendErrorProd = (err: AppError, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💥", err);
+    res.status(500).json({
+      status: `error`,
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+export const globalErrorHandler = (
+  err: AppError | ExtendedError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error: AppError | ExtendedError = { ...err };
+
+    if ((err as ExtendedError).name === "CastError") {
+      error = handleCastErrorDB(error);
+    } else if ((err as ExtendedError).code === 11000) {
+      error = handleDuplicatedFieldsDB(error);
+    } else if ((err as ExtendedError).name === "ValidationError") {
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
+};
