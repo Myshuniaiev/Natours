4edb7db9beba0fd5ce031d9e91752ec968508224
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-20 14:18:53 -0400

    fixing the signup controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,7 +4,12 @@ import catchAsync from "../utils/catchAsync";
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const user = await User.create(req.body);
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
 
     res.status(201).json({
       status: "success",
