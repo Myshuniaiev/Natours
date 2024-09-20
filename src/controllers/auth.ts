0fe9431fb2a5370fce0be3d0398128258171db16
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-20 14:47:36 -0400

    modified env file with jwt config details; modified auth controller with jwt token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
-import { NextFunction, Request, Response } from "express";
-import User, { IUser } from "../models/user";
+import jwt from "jsonwebtoken";
 import catchAsync from "../utils/catchAsync";
+import User, { IUser } from "../models/user";
+import { NextFunction, Request, Response } from "express";
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -11,8 +12,13 @@ export const signup = catchAsync(
       passwordConfirm: req.body.passwordConfirm,
     });
 
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION_TIME,
+    });
+
     res.status(201).json({
       status: "success",
+      token,
       data: { user },
     });
   }
