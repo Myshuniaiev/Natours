d6448bee987e8c807cc4a9e1e675956540baaa98
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-22 15:13:20 -0400

    removed async code form restrictTo

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -101,22 +101,13 @@ export const protect = catchAsync(
   }
 );
 
-export const restrictTo = (...roles) =>
-  catchAsync(
-    async (
-      req: IRequestWithUser,
-      res: Response,
-      next: NextFunction
-    ): Promise<void> => {
-      if (!roles.includes(req.user.role)) {
-        return next(
-          new AppError(
-            "You do not have permission to perform this action.",
-            403
-          )
-        );
-      }
-
-      next();
+export const restrictTo =
+  (...roles) =>
+  (req: IRequestWithUser, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action.", 403)
+      );
     }
-  );
+    next();
+  };
