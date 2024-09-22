1be2b6a53ab49f59b5f6a9f6494b517ba8948437
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-09-22 15:56:48 -0400

    modified the delete controller with a message

--- src/controllers/tour.ts
+++ src/controllers/tour.ts
@@ -89,7 +89,9 @@ export const deleteTour = catchAsync(
     if (!tour) {
       return next(new AppError("No tour found with that ID", 404));
     }
-    res.status(200).json({ status: "success" });
+    res
+      .status(200)
+      .json({ status: "success", message: "The tour has been deleted." });
   }
 );
 
