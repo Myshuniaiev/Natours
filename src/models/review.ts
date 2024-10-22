d180224afb4ad2b88ddd02afdeb44453bb32dcd3
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-10-22 12:54:38 -0400

    refactoring review model; implementing average rating calculation

--- src/models/review.ts
+++ src/models/review.ts
@@ -1,6 +1,16 @@
-import mongoose, { Schema, Model, Query, Document } from "mongoose";
-
+import mongoose, { Model, Query, Schema, Types } from "mongoose";
+import Tour from "@models/tour";
 import { IReview } from "@mytypes/review";
+
+// Extend the Model interface to include the static method
+interface ReviewModel extends Model<IReview> {
+  calcAverageRatings(tourId: Types.ObjectId): Promise<void>;
+}
+
+// Extend the Query interface to include the 'r' property
+interface QueryWithR extends Query<IReview, IReview> {
+  r?: IReview | null;
+}
 
 // Schema definition
 const reviewSchema: Schema<IReview> = new mongoose.Schema(
@@ -34,13 +44,69 @@ const reviewSchema: Schema<IReview> = new mongoose.Schema(
   }
 );
 
-// Query middleware
-reviewSchema.pre(/^find/, function (this: Query<IReview, Document>, next) {
+// Query middleware to populate user information
+reviewSchema.pre<Query<IReview, IReview>>(/^find/, function (next) {
   this.populate({ path: "user", select: "name photo" });
   next();
 });
 
-// Model creation
-const Review: Model<IReview> = mongoose.model<IReview>("Review", reviewSchema);
+// Static method to calculate average ratings
+reviewSchema.statics.calcAverageRatings = async function (
+  tourId: Types.ObjectId
+) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: 0,
+      ratingsAverage: 0, // Default average rating
+    });
+  }
+};
+
+// Middleware to calculate average ratings after saving a review
+reviewSchema.post<IReview>("save", async function () {
+  await (this.constructor as typeof Review).calcAverageRatings(
+    this.tour as Types.ObjectId
+  );
+});
+
+// Middleware to store the document before an update in pre-findOneAnd
+reviewSchema.pre<QueryWithR>(/^findOneAnd/, async function (next) {
+  const query = this.getQuery();
+  this.r = await this.model.findOne(query);
+  next();
+});
+
+// Middleware to update the average rating after findOneAnd operations
+reviewSchema.post<QueryWithR>(/^findOneAnd/, async function () {
+  if (this.r) {
+    await (this.r.constructor as ReviewModel).calcAverageRatings(
+      this.r.tour as Types.ObjectId
+    );
+  }
+});
+
+// Create the Review model
+const Review: ReviewModel = mongoose.model<IReview, ReviewModel>(
+   "Review",
+  reviewSchema
+);
 
 export default Review;
